7f56c038455e5c4309b258fce2452be94b2b1b09
mrkcl123 <github@mrkcl.de>
2026-05-14 18:38:00 +0200

    Refactor DataTreeExplorer for improved UI and functionality
    - Enhanced UIElement structure to include RightLabel for better data representation.
    - Introduced FolderGroup class for better organization of data paths.
    - Updated RegenerateTreeLayout to support new grouping logic and standalone leaves.
    - Improved AddDataLeafToUI method to handle data leaf rendering more effectively.
    - Adjusted visual elements rendering to differentiate between header groups and data leaves.
    - Modified DataTreeExplorerComponent to clear and manage expanded folders more efficiently.
    - Changed output parameter access from list to tree for better data structure handling.
    - Added todos for future enhancements and tasks related to the project.

--- obj/Debug/net48/DataTreeExplorer.AssemblyInfo.cs
+++ obj/Debug/net48/DataTreeExplorer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of DataTreeExplorer")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5f1fcbf27970a0bbe8500059daae933de9693f42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e3183bb97f429b53660a91d123f3dfb52921261c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataTreeExplorer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataTreeExplorer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
